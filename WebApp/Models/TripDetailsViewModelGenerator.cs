b6692fd1b5b0cfc5b27d7fff4c9629305e7a1c13
Jakub Lotko <1elstac1@gmail.com>
2019-03-29 21:47:29 +0100

    Costam klas i dodanie TripDetailsViewModelu

---
+++ WebApp/Models/TripDetailsViewModelGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using WebApp.Data.Repositories;
+using WebApp.ViewModels;
+
+namespace WebApp.Models
+{
+    public interface ITripDetailsViewModelGenerator
+    {
+        TripDetailsViewModel GetViewModel(int tripId, int type);
+    }
+    public class TripDetailsViewModelGenerator : ITripDetailsViewModelGenerator
+    {
+        private ITripDetailsRepository detailsRepository;
+
+        public TripDetailsViewModelGenerator(ITripDetailsRepository detailsRepository)
+        {
+            this.detailsRepository = detailsRepository;
+        }
+
+        public TripDetailsViewModel GetViewModel(int tripId, int type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WebApp/ViewModels/TripDetailsViewModel.cs
+++ WebApp/ViewModels/TripDetailsViewModel.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-
-namespace WebApp.Data
+using WebApp.Data;
+namespace WebApp.ViewModels
 {
-    public class TripDetails:BaseEntity
+    public class TripDetailsViewModel
     {
         public Address DestinationAddress { get; set; }
         public Address StartingAddress { get; set; }
-        public int DriverId { get; set; }
         public float Cost { get; set; }
         public string Description { get; set; }
         public string VechicleModel { get; set; }
         public DateTime Date { get; set; }
+        public string DriverUsername { get; set; }
+        public int Size { get; set; }
     }
 }
