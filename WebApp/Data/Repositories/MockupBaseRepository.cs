52a5ceacf7d778dc0c9e10782888afbefa518dc0
Jakub Lotko <1elstac1@gmail.com>
2019-03-29 12:20:12 +0100

    Dodanie MockupBaseRepository jako baza danych na testy

---
+++ WebApp/Data/Repositories/MockupBaseRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApp.Data.Repositories
+{
+    /// <summary>
+    /// Base class for every mockup repository. Every mockupRepository implementation should inherit from this class.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class MockupBaseRepository<T> : IRepository<T> where T:BaseEntity
+    {
+        protected List<T> dbContext;
+
+        public void Add(T toAdd)
+        {
+            dbContext.Add(toAdd);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return dbContext;
+        }
+
+        public T GetById(int id)
+        {
+            return dbContext[id];
+        }
+
+        public IEnumerable<T> GetList(ISpecification<T> specification)
+        {
+            return dbContext.AsQueryable().Where(specification.Criteria);
+        }
+
+        public void Remove(T toRemove)
+        {
+            dbContext.Remove(toRemove);
+        }
+    }
+}
