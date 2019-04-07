094a5c26939cc27a8d7dc5d74fc79fef15f7eeae
Jakub Lotko <1elstac1@gmail.com>
2019-04-07 18:15:03 +0200

    EmailAddressValidator i testy do niego

---
+++ Tests/EmailAddressValidatorTest.cs
@@ -0,0 +1,31 @@
+﻿using WebApp.Exceptions;
+using WebApp.Models;
+using Xunit;
+
+namespace Tests
+{
+    public class EmailAddressValidatorTest
+    {
+        private EmailAddressValidator validator;
+
+        public EmailAddressValidatorTest()
+        {
+            validator = new EmailAddressValidator();
+        }
+
+        [Fact]
+        public void PassCorrectAddress()
+        {
+            Assert.True(validator.ValidateEmailAddress("test@testmail.com"));
+        }
+
+        [Theory]
+        [InlineData("@testmail.com")]
+        [InlineData("test@.com")]
+        [InlineData("test@testmail")]
+        public void ThrowException(string address)
+        {
+            Assert.Throws<InvalidEmailAddressException>(() => validator.ValidateEmailAddress(address));
+        }
+    }
+}
---
+++ WebApp/Models/EmailAddressValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public interface IEmailAddressValidator
+    {
+        bool ValidateEmailAddress(string emailAddress);
+    }
+
+    public class EmailAddressValidator : IEmailAddressValidator
+    {
+        public bool ValidateEmailAddress(string emailAddress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
