1d066e45656bdc8754e97d4059be7c6edcb1da60
Jakub Lotko <1elstac1@gmail.com>
2019-04-12 12:16:56 +0200

    Dodanie klas

---
+++ Tests/MessageBuilderTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApp.Services;
+
+namespace Tests
+{
+    
+    class MessageBuilderTests
+    {
+        private MessageBuilder messageBuilder;
+
+
+    }
+}
---
+++ WebApp/Services/MessageBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Services
+{
+    public interface IMessageBuilder
+    {
+        void SetTemplate(string template);
+        void SetHead(string text);
+        void SetFooter(string text);
+        void SetBody(string text);
+    }
+
+    public class MessageBuilder : IMessageBuilder
+    {
+        public void SetBody(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetFooter(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetHead(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetTemplate(string template)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
