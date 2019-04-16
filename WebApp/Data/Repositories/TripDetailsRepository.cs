59fd494710f8078eb23b6d626b0b3137d4600ad1
Jakub Lotko <1elstac1@gmail.com>
2019-04-16 14:11:08 +0200

    Dziedziczenie z BaseRepository w TripDetailsRepository

--- WebApp/Data/Repositories/TripDetailsRepository.cs
+++ WebApp/Data/Repositories/TripDetailsRepository.cs
@@ -12,40 +12,10 @@ namespace WebApp.Data.Repositories
 
     }
 
-    public class TripDetailsRepository:ITripDetailsRepository
+    public class TripDetailsRepository:BaseRepository<TripDetails>, ITripDetailsRepository
     {
-        private ApplicationContext dbContext;
-
-        public TripDetailsRepository(ApplicationContext Context)
+        public TripDetailsRepository(ApplicationContext context):base(context)
         {
-            dbContext = Context;
-        }
-
-        public void Add(TripDetails toAdd)
-        {
-            dbContext.Set<TripDetails>().Add(toAdd);
-             dbContext.SaveChanges();
-        }
-
-        public IEnumerable<TripDetails> GetAll()
-        {
-            return dbContext.Set<TripDetails>().ToList();
-        }
-
-        public TripDetails GetById(int id)
-        {
-            return dbContext.Set<TripDetails>().Find(id);
-        }
-
-        public IEnumerable<TripDetails> GetList(ISpecification<TripDetails> specification)
-        {
-            return dbContext.Set<TripDetails>().AsQueryable().Where(specification.Criteria).ToList();
-        }
-
-        public void Remove(TripDetails toRemove)
-        {
-            dbContext.Set<TripDetails>().Remove(toRemove);
-            dbContext.SaveChanges();
         }
     }
 }
