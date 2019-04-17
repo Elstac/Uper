88339b05eedd26cca96e4c47502de529c3bc7994
Jakub Lotko <1elstac1@gmail.com>
2019-04-17 08:40:24 +0200

    Dodanie kontrolera i specyfikacji pobierania danych do zwracania do API

---
+++ WebApp/Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApp.Data;
+using WebApp.Data.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class ApiController : Controller
+    {
+        ITripDetailsRepository repository;
+
+        public ApiController(ITripDetailsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return repository.GetList()
+        }
+    }
+}
---
+++ WebApp/Data/Specifications/ApiTripDetailsSpecification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace WebApp.Data.Specifications
+{
+    public class ApiTripDetailsSpecification : ISpecification<TripDetails>
+    {
+        public Expression<Func<TripDetails, bool>> Criteria { get; };
+
+        public Expression<Func<TripDetails, object>> OrderBy => throw new NotImplementedException();
+
+        public Expression<Func<TripDetails, object>> OrderByDescending => throw new NotImplementedException();
+
+        public int Take { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
