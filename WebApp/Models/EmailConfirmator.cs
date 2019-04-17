ac339779e9a7fecd5968266c9428e2937b0d73f4
Jakub Lotko <1elstac1@gmail.com>
2019-04-17 12:11:42 +0200

    Email confirmator

---
+++ WebApp/Models/EmailConfirmator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Data;
+using WebApp.Services;
+
+namespace WebApp.Models
+{
+    public interface IEmailConfirmator
+    {
+        Task SendConfirmationEmailAsync(ApplicationUser user, string url);
+    }
+
+    public class EmailConfirmator : IEmailConfirmator
+    {
+        private IEmailService emailService;
+        private UserManager<ApplicationUser> userManager;
+
+        public EmailConfirmator(IEmailService emailService, UserManager<ApplicationUser> userManager)
+        {
+            this.emailService = emailService;
+            this.userManager = userManager;
+        }
+
+        public async Task SendConfirmationEmailAsync(ApplicationUser user,string url)
+        {
+            var token = await userManager.GenerateEmailConfirmationTokenAsync(user);
+            emailService.SendMail("Uper", user.Email, "AccountConfirmation",
+                new MessageBody().AddReplacement($"{user.UserName}", "{Name}")
+                .AddReplacement($"<a href={url}?userId={user.Id}&token={token}>this link</a>", "{Link}"));
+        }
+    }
+}
