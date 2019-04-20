6ecb7aa6bee1806d0c2276fbe06cdce2cc9996a8
Jakub Lotko <1elstac1@gmail.com>
2019-04-20 17:06:18 +0200

    Implementacja fabryki dla resetowania hasla

--- WebApp/Models/EmailConfirmation/PasswordResetFactory.cs
+++ WebApp/Models/EmailConfirmation/PasswordResetFactory.cs
@@ -1,11 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WebApp.Data.Repositories;
+using WebApp.Services;
+using Microsoft.Extensions.DependencyInjection;
+using WebApp.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace WebApp.Models.EmailConfirmation
 {
-    public class PasswordResetFactory
+    public interface IPasswordResetConfirmatorFactory
     {
+        IEmailConfirmationSender CreateAccountConfirmatorSender(string name);
+        IEmailConfirmator CreateAccountConfirmator();
+    }
+
+    public class PasswordResetConfirmatorFactory : IPasswordResetConfirmatorFactory
+    {
+        private IServiceProvider serviceProvider;
+
+        public PasswordResetConfirmatorFactory(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public IEmailConfirmator CreateAccountConfirmator()
+        {
+            return new EmailConfirmator(new PasswordResetConfirmationProvider(serviceProvider.GetService<UserManager<ApplicationUser>>()),
+                                        serviceProvider.GetService<IApplicationUserRepository>());
+        }
+
+        public IEmailConfirmationSender CreateAccountConfirmatorSender(string name)
+        {
+            var body = new MessageBodyDictionary();
+            body.AddReplacement(name, "{Name}")
+                .AddReplacement("to reset your password", "{Purpose}");
+
+            return new EmailConfirmatorSender(serviceProvider.GetService<IEmailService>(),
+                body,
+                serviceProvider.GetService<IApplicationUserRepository>(),
+                new PasswordResetTokenProvider(serviceProvider.GetService<UserManager<ApplicationUser>>()),
+                "HyperlinkConfirmation");
+        }
     }
 }
