f05069ba1dca7c5b997b754b18495dea918ad5b3
Jakub Lotko <1elstac1@gmail.com>
2019-04-20 16:48:21 +0200

    Dodanie passwordResetProvidera

---
+++ WebApp/Models/EmailConfirmation/PasswordResetProvider.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading.Tasks;
+using WebApp.Data;
+
+namespace WebApp.Models.EmailConfirmation
+{
+    public class PasswordResetProvider : IConfirmationProvider
+    {
+        private UserManager<ApplicationUser> userManager;
+
+        public Task ConfirmAsync(ApplicationUser user, string token, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
