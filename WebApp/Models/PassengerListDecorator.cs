26bcb382c4df74637d4f76bc7dd8e9b9afa06b43
Dominik Snarski <dominik.snarski@gmail.com>
2019-04-23 15:57:37 +0200

    Few fixes

--- WebApp/Models/PassengerListDecorator.cs
+++ WebApp/Models/PassengerListDecorator.cs
@@ -20,7 +20,7 @@ namespace WebApp.Models
         {
             var vm = wrape.CreateViewModel(tripDetails);
             
-            vm.PassangersUsernames = (from tu in tripDetails.Passangers select tu.User.Name).ToList();
+            vm.PassangersUsernames = (from tu in tripDetails.Passangers select tu.User.UserName).ToList();
 
             return vm;
         }
