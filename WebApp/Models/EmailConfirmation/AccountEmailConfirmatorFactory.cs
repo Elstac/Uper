f3bd3fef3aa58b7117eb7f3c330d6adfe1c5b9cd
Jakub Lotko <1elstac1@gmail.com>
2019-04-26 14:41:11 +0200

    Dodanie implementacji EmailSendera dla potwierdzania konta

---
+++ WebApp/Models/EmailConfirmation/AccountEmailConfirmatorFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using WebApp.Data.Repositories;
+using WebApp.Services;
+using Microsoft.Extensions.DependencyInjection;
+using WebApp.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebApp.Models.EmailConfirmation
+{
+    public interface IAccountEmailConfirmatorFactory
+    {
+        IEmailConfirmationSender CreateAccountConfirmatorSender(string name);
+        IEmailConfirmator CreateAccountConfirmator();
+    }
+
+    public class AccountEmailConfirmatorFactory:IAccountEmailConfirmatorFactory
+    {
+        private IServiceProvider serviceProvider;
+
+        public AccountEmailConfirmatorFactory(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public IEmailConfirmator CreateAccountConfirmator()
+        {
+            return new EmailConfirmator(new AccountConfirmationProvider(), 
+                                        serviceProvider.GetService<IApplicationUserRepository>());
+        }
+
+        public IEmailConfirmationSender CreateAccountConfirmatorSender(string name)
+        {
+            var body = new MessageBodyDictionary();
+            body.AddReplacement(name, "{Name}")
+                .AddReplacement("to confirm your account", "{Purpose}");
+
+            return new EmailConfirmationSender(serviceProvider.GetService<IEmailService>(),
+                body,
+                serviceProvider.GetService<IApplicationUserRepository>(),
+                new AccountTokenProvider(serviceProvider.GetService<UserManager<ApplicationUser>>()),
+                "HyperLinkConfirmation");
+        }
+    }
+}
