11c281d8ed4ee68f2356aabb93ac9fa3ad9e4e3a
Jakub Lotko <1elstac1@gmail.com>
2019-04-26 14:40:35 +0200

    Implementacja Confirmatora dla potwierdzania konta

---
+++ WebApp/Models/EmailConfirmation/AccountConfirmationProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using WebApp.Data;
+
+namespace WebApp.Models.EmailConfirmation
+{
+    public interface IConfirmationProvider
+    {
+        Task ConfirmAsync(ApplicationUser user, string token, params object[] parameters);
+    }
+
+    public class AccountConfirmationProvider : IConfirmationProvider
+    {
+        private UserManager<ApplicationUser> userManager;
+
+        public async Task ConfirmAsync(ApplicationUser user, string token, params object[] parameters)
+        {
+            await userManager.ConfirmEmailAsync(user, token);
+        }
+    }
+}
