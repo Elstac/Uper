f5447f016dc97fa93f5441b77698606040df7f71
Jakub Lotko <1elstac1@gmail.com>
2019-04-26 14:22:40 +0200

    Dodanie wlasniego wyjatku OwnNullArgumentException

---
+++ WebApp/Exceptions/OwnNullArgumentException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Exceptions
+{
+    public class OwnNullArgumentException : ArgumentNullException
+    {
+        public OwnNullArgumentException(string message):base(message)
+        {
+        }
+    }
+}
