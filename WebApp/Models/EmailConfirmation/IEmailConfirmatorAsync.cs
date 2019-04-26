aa3e49c4ffac1aa20de574049426fdfa852c451c
Jakub Lotko <1elstac1@gmail.com>
2019-04-26 14:30:08 +0200

    Usuniecie IEmailConfirmatoraAsync z odzielnego pliku

--- WebApp/Models/EmailConfirmation/IEmailConfirmatorAsync.cs
+++ WebApp/Models/EmailConfirmation/IEmailConfirmatorAsync.cs
@@ -2,9 +2,5 @@
 
 namespace WebApp.Models.EmailConfirmation
 {
-    interface IEmailConfirmatorAsync
-    {
-        void SendConfirmationEmail(string Id, string url);
-        Task ConfirmEmailAsync(string Id, string token, params object[] parameters);
-    }
+    
 }
