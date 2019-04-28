2e165037de66e4f789917a9d73824c333252005c
Jakub Lotko <1elstac1@gmail.com>
2019-04-28 19:54:45 +0200

    Dziedziczenie AccountEmailConfirmatorFactory z bazowej fabtyki

--- WebApp/Models/Factories/AccountEmailConfirmatorFactory.cs
+++ WebApp/Models/Factories/AccountEmailConfirmatorFactory.cs
@@ -1,40 +1,18 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using WebApp.Data.Repositories;
-using WebApp.Services;
+﻿using System;
 
 namespace WebApp.Models.EmailConfirmation
 {
-    public interface IAccountEmailConfirmatorFactory
+    public interface IAccountEmailConfirmatorFactory:IStandardEmailConfirmatorFactory
     {
-        IEmailConfirmationSender CreateAccountConfirmatorSender(string name);
-        IEmailConfirmator CreateAccountConfirmator();
     }
 
-    public class AccountEmailConfirmatorFactory:IAccountEmailConfirmatorFactory
+    class AccountEmailConfirmatorFactory : StandardEmailConfirmatorFactory, IAccountEmailConfirmatorFactory
     {
-        private IServiceProvider serviceProvider;
-        private IMessageBodyProvider messageBodyProvider;
-
         public AccountEmailConfirmatorFactory(IServiceProvider serviceProvider)
+            : base(serviceProvider,
+                  ConfirmatorType.Account, 
+                  "HyperlinkConfirmation")
         {
-            this.serviceProvider = serviceProvider;
-            messageBodyProvider = new AccountMessageProvider();
-        }
-
-        public IEmailConfirmator CreateAccountConfirmator()
-        {
-            return new EmailConfirmator(serviceProvider.GetService<AccountConfirmationProvider>(),
-                                        serviceProvider.GetService<IApplicationUserRepository>());
-        }
-
-        public IEmailConfirmationSender CreateAccountConfirmatorSender(string name)
-        {
-            return new EmailConfirmatorSender(serviceProvider.GetService<IEmailService>(),
-                messageBodyProvider.GetBody(),
-                serviceProvider.GetService<IApplicationUserRepository>(),
-                serviceProvider.GetService<AccountTokenProvider>(),
-                "HyperlinkConfirmation");
         }
     }
 }
