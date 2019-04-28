2657cfd8b5941c09cf0f2b161dcd15a336af4b49
Jakub Lotko <1elstac1@gmail.com>
2019-04-28 11:28:40 +0200

    Dodanie dwoch implementacji IMessageBodyProvider

---
+++ WebApp/Models/EmailConfirmation/PasswordResetMessageProvider.cs
@@ -0,0 +1,14 @@
+﻿using WebApp.Services;
+
+namespace WebApp.Models.EmailConfirmation
+{
+    public class PasswordResetMessageProvider : IMessageBodyProvider
+    {
+        public IMessageBodyDictionary GetBody(params object[] par)
+        {
+            return new MessageBodyDictionary()
+                .AddReplacement(par[0].ToString(), "{Name}")
+                .AddReplacement("to reset your password", "{Purpose}");
+        }
+    }
+}
