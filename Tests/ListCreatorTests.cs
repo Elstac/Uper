a58a772240672de76f24fa98adafbd7eed3d3432
MPiotrowski <matipio98@wp.pl>
2019-04-29 13:30:18 +0200

    List Creator + testy (jeden trzeba poprawic ale dobrze dziala)

---
+++ Tests/ListCreatorTests.cs
@@ -0,0 +1,114 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApp.Data;
+using WebApp.Models.OfferList;
+using Xunit;
+
+namespace Tests
+{
+    public class ListCreatorTests
+    {
+        [Fact]
+        public void ListCreator_Test()
+        {
+            var repository = new Mock<IRepository<TripDetails,int>>();
+            List<TripDetails> testQuery = new List<TripDetails>();
+            TripDetails trip = new TripDetails()
+            {
+                Cost = 100,
+                StartingAddress = new Address() { City = "Kair" },
+                DestinationAddress = new Address { City = "Moskwa" }
+            };
+            testQuery.Add(trip);
+            trip = new TripDetails()
+            {
+                Cost = 210.50f,
+                StartingAddress = new Address() { City = "kAIr" },
+                DestinationAddress = new Address { City = "moSkWa" }
+            };
+            testQuery.Add(trip);
+            trip = new TripDetails()
+            {
+                Cost = 324.86f,
+                StartingAddress = new Address() { City = "Kair" },
+                DestinationAddress = new Address { City = "Hong Kong" }
+            };
+            testQuery.Add(trip);
+            trip = new TripDetails()
+            {
+                Cost = 250.00f,
+                StartingAddress = new Address() { City = "Kair" },
+                DestinationAddress = new Address { City = "MosKwa" }
+            };
+            testQuery.Add(trip);
+            trip = new TripDetails()
+            {
+                Cost = 205.65f,
+                StartingAddress = new Address() { City = "Helsinki" },
+                DestinationAddress = new Address { City = "Berlin" }
+            };
+            testQuery.Add(trip);
+            IEnumerable<TripDetails> testQuery_ = testQuery;
+
+            repository
+                .Setup(m=>m.GetAll())
+                .Returns(testQuery_);
+            ListCreator list = new ListCreator(repository.Object);
+
+            IQueryable<TripDetails> travelList = list.GetList(210.50f, "Kair", "MoSkwa");
+            
+            List<TripDetails> modelTravelList_ = new List<TripDetails>();
+            trip = new TripDetails()
+            {
+                Cost = 100,
+                StartingAddress = new Address() { City = "Kair" },
+                DestinationAddress = new Address { City = "Moskwa" }
+            };
+            testQuery.Add(trip);
+            trip = new TripDetails()
+            {
+                Cost = 210.50f,
+                StartingAddress = new Address() { City = "kAIr" },
+                DestinationAddress = new Address { City = "moSkWa" }
+            };
+            testQuery.Add(trip);
+            IEnumerable<TripDetails> modelTravelList__ = modelTravelList_;
+            IQueryable<TripDetails> modelTravelList = modelTravelList__.AsQueryable();
+
+            Assert.Equal(modelTravelList, travelList);
+        }
+
+        [Fact]
+        public void ListCreator_listIsEmpty()
+        {
+            var repository = new Mock<IRepository<TripDetails, int>>();
+            List<TripDetails> testQuery = new List<TripDetails>();
+            TripDetails trip = new TripDetails()
+            {
+                Cost = 100,
+                StartingAddress = new Address() { City = "Kair" },
+                DestinationAddress = new Address { City = "Moskwa" }
+            };
+            testQuery.Add(trip);
+            trip = new TripDetails()
+            {
+                Cost = 205.65f,
+                StartingAddress = new Address() { City = "Helsinki" },
+                DestinationAddress = new Address { City = "Berlin" }
+            };
+            testQuery.Add(trip);
+            IEnumerable<TripDetails> testQuery_ = testQuery;
+
+            repository
+                .Setup(m => m.GetAll())
+                .Returns(testQuery_);
+            ListCreator list = new ListCreator(repository.Object);
+
+            IQueryable<TripDetails> travelList = list.GetList(1002.54f, "Moskwa", "Berlin");
+
+            Assert.Empty(travelList);
+        }
+    }
+}
---
+++ WebApp/Models/OfferList/ListCreator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using WebApp.Data;
+using WebApp.Data.Specifications;
+
+namespace WebApp.Models.OfferList
+{
+    interface IListCreator
+    {
+
+    }
+
+    public class ListCreator
+    {
+        IRepository<TripDetails, int> repository;
+
+        public ListCreator(IRepository<TripDetails,int> repository)
+        {
+            this.repository = repository;
+        }
+        public IQueryable<TripDetails> GetList(float MaxCost,string StartCity,string DestCity)
+        {
+            IQueryable<TripDetails> TList = repository.GetAll().AsQueryable();
+            Expression<Func<TripDetails, bool>> expression = c => c.Cost <= MaxCost && c.DestinationAddress.City.ToUpper() == DestCity.ToUpper() && c.StartingAddress.City.ToUpper() == StartCity.ToUpper();
+            ISpecification< TripDetails > spec = new BaseSpecification(expression);
+            TList = SpecificationEvaluator<TripDetails>.EvaluateSpecification(TList, spec).AsQueryable();
+            return TList;
+        }
+            
+    }
+
+    internal class BaseSpecification : BaseSpecification<TripDetails>
+    {
+        public BaseSpecification(Expression<Func<TripDetails, bool>> criteria) : base(criteria)
+        {
+        }
+    }
+}
