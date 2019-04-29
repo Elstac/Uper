d39dc938605e5f564207c918ee4c4675b3ef6e7f
Jakub Lotko <1elstac1@gmail.com>
2019-04-29 21:34:50 +0200

    Dodanie rysowania po canvasie

--- WebApp/wwwroot/js/Map.js
+++ WebApp/wwwroot/js/Map.js
@@ -1,1 +1,31 @@
-﻿
+﻿function customLine(x1,x2,y1,y2) {
+    this.x1=x1;
+    this.x2=x2;
+    this.y1=y1;
+    this.y2=y2;
+}
+
+var lastX;
+var lastY;
+var arr = new Array();
+
+function drawLine(value) {
+    line(value.x1, value.y1, value.x2, value.y2);
+}
+
+function mouseDragged() {
+    arr.push(new customLine(mouseX, lastX, mouseY, lastY));
+}
+
+function draw() {
+    background(51);
+
+    arr.forEach(drawLine);
+
+    lastX = mouseX;
+    lastY = mouseY;
+}
+
+function setup() {
+    createCanvas(400, 400);
+}
