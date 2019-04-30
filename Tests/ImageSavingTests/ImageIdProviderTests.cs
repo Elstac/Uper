8a68d93c6404a995041073322045d22d15122faa
Jakub Lotko <1elstac1@gmail.com>
2019-04-30 19:41:07 +0200

    Klasa testowa do ImageIdProvider

---
+++ Tests/ImageSavingTests/ImageIdProviderTests.cs
@@ -0,0 +1,15 @@
+﻿using WebApp.Models;
+using Xunit;
+
+namespace Tests.ImageSavingTests
+{
+    public class ImageIdProviderTests
+    {
+        private ImageIdProvider idProvider;
+
+        public ImageIdProviderTests()
+        {
+            idProvider = new ImageIdProvider();
+        }
+    }
+}
