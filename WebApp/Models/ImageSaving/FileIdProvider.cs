2c56ca6e0610cb834484d3d6baadbbc9e5416916
Jakub Lotko <1elstac1@gmail.com>
2019-04-30 21:37:47 +0200

    Wstepna implementacja FileIdProvidera

--- WebApp/Models/ImageSaving/FileIdProvider.cs
+++ WebApp/Models/ImageSaving/FileIdProvider.cs
@@ -1,4 +1,8 @@
-﻿namespace WebApp.Models
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApp.Models
 {
     public interface IFileIdProvider
     {
@@ -7,9 +11,58 @@
 
     public class FileIdProvider : IFileIdProvider
     {
-        public string GetId(string directory,string extention)
+        public string GetId(string directory, string extention)
         {
-            throw new System.NotImplementedException();
+            if (!Directory.Exists(directory))
+                throw new DirectoryNotFoundException();
+
+            var files = Directory.GetFiles(directory).ToList();
+
+            var actId = "0";
+
+            files = (from file in files
+                     where Path.GetExtension(file) == extention
+                     select file).OrderBy((file) => file).ToList();
+
+            foreach (var file in files)
+            {
+                if (Path.GetFileNameWithoutExtension(file) == actId)
+                {
+                    actId = GetNextId(actId);
+                }
+                else
+                    break;
+            }
+
+            return actId;
+        }
+
+        private string GetNextId(string actId)
+        {
+            char c;
+            string ret = "";
+
+            for (int i = 0; i < actId.Length; i++)
+            {
+                c = actId[i];
+                if(++c>122)
+                {
+                    if(i==actId.Length-1)
+                    {
+                        ret = actId.Remove(i, 1);
+                        ret = ret.Insert(i, "00");
+                        break;
+                    }
+                }
+                else
+                {
+                    ret = actId.Remove(i);
+                    ret = ret.Insert(i,c.ToString());
+                    break;
+                }
+            }
+
+            return ret;
         }
     }
 }
