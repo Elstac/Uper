3fbbe9df291442db7d28b684310f150236c6a7e0
Jakub Lotko <1elstac1@gmail.com>
2019-04-30 19:29:34 +0200

    Dodanie testu tworzenia pliku o podanej nazwie

---
+++ Tests/ImageSavingTests/ImageWriterTests.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using WebApp.Models;
+using Xunit;
+
+namespace Tests.ImageSavingTests
+{
+    public class ImageWriterTests
+    {
+        private ImageWriter imageWriter;
+
+        public ImageWriterTests()
+        {
+            imageWriter = new ImageWriter();
+        }
+
+        [Fact]
+        public void CreateFileWithGivenName()
+        {
+            imageWriter = new ImageWriter();
+
+            imageWriter.SaveImage("image.png", "");
+
+            Assert.True(File.Exists("image.png"));
+
+            File.Delete("image.png");
+        }
+    }
+}
