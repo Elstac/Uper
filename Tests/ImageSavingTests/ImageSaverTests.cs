e5824d8eb3c8fa417ad54092534de79b7ec24412
Jakub Lotko <1elstac1@gmail.com>
2019-04-30 20:45:15 +0200

    Klassa testowa do ImageSaver

---
+++ Tests/ImageSavingTests/ImageSaverTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using Moq;
+using WebApp.Models;
+
+namespace Tests.ImageSavingTests
+{
+    public class ImageSaverTests
+    {
+        private ImageSaver imageSaver;
+        private Mock<IImageIdProvider> idMock;
+        private Mock<IImageWriter> writerMock;
+
+        public ImageSaverTests()
+        {
+
+        }
+    }
+}
