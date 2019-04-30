d9a1a0a8f5fe27113034827dab7b5f1aad9f9029
Jakub Lotko <1elstac1@gmail.com>
2019-04-30 20:43:00 +0200

    Testy ImageIdProvidera

--- Tests/ImageSavingTests/ImageIdProviderTests.cs
+++ Tests/ImageSavingTests/ImageIdProviderTests.cs
@@ -1,4 +1,5 @@
-﻿using WebApp.Models;
+﻿using System.IO;
+using WebApp.Models;
 using Xunit;
 
 namespace Tests.ImageSavingTests
@@ -6,10 +7,48 @@ namespace Tests.ImageSavingTests
     public class ImageIdProviderTests
     {
         private ImageIdProvider idProvider;
+        private string path = "test/";
 
         public ImageIdProviderTests()
         {
+            Directory.CreateDirectory("test/");
+        }
+
+        [Fact]
+        public void GetFirstFreeIdIfNoFileIsPresent()
+        {
             idProvider = new ImageIdProvider();
+
+            var @out = idProvider.GetId(path, ".png");
+
+            Assert.Equal("0", @out);
+        }
+
+        [Fact]
+        public void ReturnCharacterIfAllNubersAreTaken()
+        {
+            idProvider = new ImageIdProvider();
+            for (int i = 0; i < 10; i++)
+                File.Create(path + i+".png").Close();
+
+            var @out = idProvider.GetId(path, ".png");
+
+            Assert.Equal("a", @out);
+
+            for (int i = 0; i < 10; i++)
+                File.Delete(path + i + ".png");
+        }
+
+        [Fact]
+        public void ThrowExeptionIfGivenDirectoryDoesntExists()
+        {
+            var nPath = "notExistingDir/";
+            if (Directory.Exists(nPath))
+                Directory.Delete(nPath);
+
+            idProvider = new ImageIdProvider();
+
+            Assert.Throws<DirectoryNotFoundException>(()=>idProvider.GetId(nPath, ".png"));
         }
     }
 }
--- WebApp/Models/ImageSaving/ImageIdProvider.cs
+++ WebApp/Models/ImageSaving/ImageIdProvider.cs
@@ -2,12 +2,12 @@
 {
     public interface IImageIdProvider
     {
-        string GetId();
+        string GetId(string directory,string extention);
     }
 
     public class ImageIdProvider : IImageIdProvider
     {
-        public string GetId()
+        public string GetId(string directory,string extention)
         {
             throw new System.NotImplementedException();
         }
