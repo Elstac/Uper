d6f85b3711efa93c6cafb15b320bb716d7fdcce5
Jakub Lotko <1elstac1@gmail.com>
2019-04-30 20:56:31 +0200

    Testy do ImageSave
    Sprawdzenie interakcji z zaleznosciami i tworzenie nazwy na podstawie
    otrzymanego ID

--- Tests/ImageSavingTests/ImageSaverTests.cs
+++ Tests/ImageSavingTests/ImageSaverTests.cs
@@ -1,6 +1,7 @@
 ﻿using Xunit;
 using Moq;
 using WebApp.Models;
+using System.IO;
 
 namespace Tests.ImageSavingTests
 {
@@ -12,7 +13,34 @@ namespace Tests.ImageSavingTests
 
         public ImageSaverTests()
         {
+            Directory.CreateDirectory("test/");
+        }
 
+        [Fact]
+        public void GetIdInGivenDirectoryAndFileExtention()
+        {
+            idMock = new Mock<IImageIdProvider>();
+            writerMock = new Mock<IImageWriter>();
+
+            imageSaver = new ImageSaver(idMock.Object, writerMock.Object);
+
+            imageSaver.SaveImage("aaa", ".png","test/");
+
+            idMock.Verify(im => im.GetId("test/", ".png"));
+        }
+
+        [Fact]
+        public void PassFileNameBasedOnRecivedIdAndImageDataToWriter()
+        {
+            idMock = new Mock<IImageIdProvider>();
+            idMock.Setup(im => im.GetId(It.IsAny<string>(), It.IsAny<string>())).Returns("id");
+            writerMock = new Mock<IImageWriter>();
+
+            imageSaver = new ImageSaver(idMock.Object, writerMock.Object);
+
+            imageSaver.SaveImage("aaa", ".png", "test/");
+
+            writerMock.Verify(wm => wm.SaveImage("id.png", "aaa"));
         }
     }
 }
