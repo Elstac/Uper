10591bbde1a10d9b58f74992dd892429fead323a
Jakub Lotko <1elstac1@gmail.com>
2019-05-01 14:02:03 +0200

    Dodanie testow ImageManager

---
+++ Tests/ImageSavingTests/ImageManagerTests.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using WebApp.Models.ImageManagement;
+using Xunit;
+namespace Tests.ImageSavingTests
+{
+    public class ImageManagerTests
+    {
+        private PngImageManager imageManager;
+        private Mock<IImageSaver> saverMock;
+        private Mock<IImageRemover> removerMock;
+
+        [Fact]
+        public void CallSaverForSavingFile()
+        {
+            saverMock = new Mock<IImageSaver>();
+            removerMock = new Mock<IImageRemover>();
+
+            imageManager = new PngImageManager(saverMock.Object, removerMock.Object);
+
+            imageManager.SaveImage("data", "test/");
+
+            saverMock.Verify(sm => sm.SaveImage("data", ".png", "test/"));
+        }
+
+        [Fact]
+        public void CallRemoverForRemovingFile()
+        {
+            saverMock = new Mock<IImageSaver>();
+            removerMock = new Mock<IImageRemover>();
+
+            imageManager = new PngImageManager(saverMock.Object, removerMock.Object);
+
+            imageManager.RemoveImage("id", "test/");
+
+            removerMock.Verify(sm => sm.RemoveImage("id","test/",".png"));
+        }
+    }
+}
---
+++ Tests/ImageSavingTests/ImageRemoverTEsts.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using WebApp.Models.ImageManagement;
+using Xunit;
+namespace Tests.ImageSavingTests
+{
+    public class ImageRemoverTests: System.IDisposable
+    {
+        private ImageRemover imageRemover;
+        private string testPath = "test/";
+
+        public ImageRemoverTests()
+        {
+            imageRemover = new ImageRemover();
+            Directory.CreateDirectory(testPath);
+        }
+
+        public void Dispose()
+        {
+            Directory.Delete(testPath, true);
+        }
+
+        [Fact]
+        public void RemoveOnlyFileWithGivenId()
+        {
+            imageRemover = new ImageRemover();
+            File.Create(testPath + "0.png").Close();
+            File.Create(testPath + "1.png").Close();
+            
+            imageRemover.RemoveImage("0", testPath, ".png");
+
+            Assert.True(!File.Exists(testPath + "0.png"));
+            Assert.True(File.Exists(testPath + "1.png"));
+
+            File.Delete(testPath + "1.png");
+        }
+    }
+}
