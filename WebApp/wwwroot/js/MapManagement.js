66497733420be29d7ce87b19d8b61763b08f9e41
Jakub Lotko <1elstac1@gmail.com>
2019-05-02 00:42:28 +0200

    Scrypt do dodawania/usuwania mapy z view tworzenia

---
+++ WebApp/wwwroot/js/MapManagement.js
@@ -0,0 +1,15 @@
+﻿function showMap() {
+    document.getElementById('mapDiv').style.display = "inline";
+    document.getElementById('removeBtn').style.display = "inline";
+
+    document.getElementById('confirmBtn').onclick = function () { saveCnv() };
+    document.getElementById('createBtn').style.display = "none";
+}
+
+function hideMap() {
+    document.getElementById('confirmBtn').onclick = function () { };
+    document.getElementById('createBtn').style.display = "inline";
+
+    document.getElementById('mapDiv').style.display = "none";
+    document.getElementById('removeBtn').style.display = "none";
+}
