48de6965864b5ef600c96c266250c6d884b88fc5
Jakub Lotko <1elstac1@gmail.com>
2019-05-01 13:52:13 +0200

    Dodanie argumentu z rozszerzeniem przy usuwania obrazu

--- WebApp/Models/ImageManagement/ImageRemover.cs
+++ WebApp/Models/ImageManagement/ImageRemover.cs
@@ -4,11 +4,11 @@ namespace WebApp.Models.ImageManagement
 {
     public interface IImageRemover
     {
-        void RemoveImage(string imageId, string directory);
+        void RemoveImage(string imageId, string directory,string fileExtention);
     }
     public class ImageRemover : IImageRemover
     {
-        public void RemoveImage(string imageId, string directory)
+        public void RemoveImage(string imageId, string directory, string fileExtention)
         {
             throw new NotImplementedException();
         }
