99d8764260da1f2d718feb96f1631118d76a0da8
MPiotrowski <matipio98@wp.pl>
2019-05-01 16:41:57 +0200

    dodanie daty do listcreatora i poprawki testów

--- WebApp/Models/OfferList/ListCreator.cs
+++ WebApp/Models/OfferList/ListCreator.cs
@@ -2,27 +2,28 @@
 using System.Linq;
 using System.Linq.Expressions;
 using WebApp.Data;
+using WebApp.Data.Repositories;
 using WebApp.Data.Specifications;
 
 namespace WebApp.Models.OfferList
 {
-    interface IListCreator
+    public interface IListCreator
     {
-
+        IQueryable<TripDetails> GetList(float MaxCost, string StartCity, string DestCity,DateTime Mindate,DateTime MaxDate);
     }
 
-    public class ListCreator
+    public class ListCreator : IListCreator
     {
-        IRepository<TripDetails, int> repository;
+        ITripDetailsRepository repository;
 
-        public ListCreator(IRepository<TripDetails,int> repository)
+        public ListCreator(ITripDetailsRepository repository)
         {
             this.repository = repository;
         }
-        public IQueryable<TripDetails> GetList(float MaxCost,string StartCity,string DestCity)
+        public IQueryable<TripDetails> GetList(float MaxCost,string StartCity,string DestCity,DateTime MinDate,DateTime MaxDate)
         {
             IQueryable<TripDetails> TList = repository.GetAll().AsQueryable();
-            Expression<Func<TripDetails, bool>> expression = c => c.Cost <= MaxCost && c.DestinationAddress.City.ToUpper() == DestCity.ToUpper() && c.StartingAddress.City.ToUpper() == StartCity.ToUpper();
+            Expression<Func<TripDetails, bool>> expression = c => c.Cost <= MaxCost && c.DestinationAddress.City.ToUpper() == DestCity.ToUpper() && c.StartingAddress.City.ToUpper() == StartCity.ToUpper() && c.Date >= MinDate && c.Date <= MaxDate;
             ISpecification< TripDetails > spec = new BaseSpecification(expression);
             TList = SpecificationEvaluator<TripDetails>.EvaluateSpecification(TList, spec).AsQueryable();
             return TList;
