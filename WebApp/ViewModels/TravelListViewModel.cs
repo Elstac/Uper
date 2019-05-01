9a7f0b0ec2e8709a4265ea6b6ccdb564179de72c
MPiotrowski <matipio98@wp.pl>
2019-05-01 23:06:56 +0200

    porządki

--- WebApp/ViewModels/TravelListViewModel.cs
+++ WebApp/ViewModels/TravelListViewModel.cs
@@ -10,21 +10,7 @@ namespace WebApp.ViewModels
         {
             this.List = List;
         }
-
-        public TravelListViewModel(string StartCity,string DestCity,float Cost,DateTime MinDate,DateTime MaxDate,IQueryable<TripDetails> List)
-        {
-            this.MinDate = MinDate;
-            this.MaxDate = MaxDate;
-            this.StartCity = StartCity;
-            this.DestCity = DestCity;
-            this.Cost = Cost;
-            this.List = List;
-        }
-        public string DestCity { get; set; }
-        public string StartCity { get; set; }
-        public float Cost { get; set; }
-        public DateTime MinDate { get; set; }
-        public DateTime MaxDate { get; set; }
+        
         public IQueryable<TripDetails> List { get; set; }
     }
 }
