6ca1da8342d01bdf41a42cf0838bae0209906766
Jakub Lotko <1elstac1@gmail.com>
2019-05-01 11:31:20 +0200

    Opis IImageSaver

--- WebApp/Models/ImageSaving/ImageSaver.cs
+++ WebApp/Models/ImageSaving/ImageSaver.cs
@@ -2,8 +2,18 @@
 
 namespace WebApp.Models
 {
+    /// <summary>
+    /// Provides saving imageData form string to image file
+    /// </summary>
     public interface IImageSaver
     {
+        /// <summary>
+        /// Save endoced image data string to new file
+        /// </summary>
+        /// <param name="imageData">string containing encoded image data</param>
+        /// <param name="fileExtention">Extention of created file</param>
+        /// <param name="directory">Path of directory where file will be saved</param>
+        /// <returns></returns>
         string SaveImage(string imageData, string fileExtention, string directory);
     }
     public class ImageSaver : IImageSaver
