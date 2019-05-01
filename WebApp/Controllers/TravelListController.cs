43fbf0f1494c2d0434f34ae4a2106b8759802755
MPiotrowski <matipio98@wp.pl>
2019-05-01 22:55:43 +0200

    lista z linkami do ofert

--- WebApp/Controllers/TravelListController.cs
+++ WebApp/Controllers/TravelListController.cs
@@ -2,7 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using WebApp.Data;
+using WebApp.Data.Repositories;
+using WebApp.Data.Specifications;
 using WebApp.Models.OfferList;
 using WebApp.ViewModels;
 
@@ -10,11 +13,11 @@ namespace WebApp.Controllers
 {
     public class TravelListController : Controller
     {
-        IListCreator listCreator;
+        ITripDetailsRepository repository;
 
-        public TravelListController(IListCreator listCreator)
+        public TravelListController(ITripDetailsRepository repository)
         {
-            this.listCreator = listCreator;
+            this.repository = repository;
         }
 
         [HttpGet]
@@ -24,10 +27,21 @@ namespace WebApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(string StartCity,string DestCity,DateTime MinDate, DateTime MaxDate,float Cost,int? page)
+        public IActionResult Index(string StartCity,string DestCity,DateTime MinDate, DateTime MaxDate,float Cost,bool Smoking,int? page)
         {
             page = page ?? 1;
-            IQueryable < TripDetails > List = listCreator.GetList(Cost, StartCity, DestCity,MinDate,MaxDate);
+            Expression<Func<TripDetails, bool>> criteria =
+                c => c.Cost <= Cost && c.DestinationAddress.City.ToUpper() == DestCity.ToUpper() &&
+                c.StartingAddress.City.ToUpper() == StartCity.ToUpper() &&
+                c.Date >= MinDate &&
+                c.Date <= MaxDate;
+            if(Smoking)
+            {
+                var com = criteria.Compile();
+                criteria = c => com(c) && c.IsSmokingAllowed == Smoking;
+            }
+            ISpecification<TripDetails> Specification = new TravelListSpecification(criteria);
+            IQueryable < TripDetails > List = repository.GetList(Specification).AsQueryable();
             TravelListViewModel vm = new TravelListViewModel(List);
             return View(vm);
         }
