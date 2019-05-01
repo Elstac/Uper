eef1af77c31635ca7214bb7bd3ab7fadc4552b88
MPiotrowski <matipio98@wp.pl>
2019-05-01 20:17:46 +0200

    dodanie specyfikacji listy podroz i linkow

---
+++ WebApp/Data/Specifications/TravelListSpecification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace WebApp.Data.Specifications
+{
+    public class TravelListSpecification : BaseSpecification<TripDetails>
+    {
+        public TravelListSpecification(Expression<Func<TripDetails, bool>> criteria) : base(criteria)
+        {
+        }
+    }
+}
