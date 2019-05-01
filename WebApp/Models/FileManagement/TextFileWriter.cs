9a523608c2ae5a87e511fa797fce0847c74c2d01
Jakub Lotko <1elstac1@gmail.com>
2019-05-01 16:00:36 +0200

    Implementacja TextFileWritera

---
+++ WebApp/Models/FileManagement/TextFileWriter.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace WebApp.Models.FileManagement
+{
+    public class TextFileWriter : IFileWriter
+    {
+        public void SaveFile(string fileName, string fileData)
+        {
+            using (var fs = new FileStream(fileName, FileMode.Create))
+            {
+                using (var sw = new StreamWriter(fs))
+                {
+                    sw.Write(fileData);
+                    sw.Close();
+                }
+                fs.Close();
+            }
+        }
+    }
+}
