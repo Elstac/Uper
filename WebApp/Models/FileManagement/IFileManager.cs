936a13da9687a5a1c2cf9431881bae7c91c0a810
Jakub Lotko <1elstac1@gmail.com>
2019-05-02 19:15:49 +0200

    Usuniecie czytania pliku z JsonManagera

--- WebApp/Models/FileManagement/IFileManager.cs
+++ WebApp/Models/FileManagement/IFileManager.cs
@@ -4,26 +4,19 @@
     {
         void RemoveFile(string id,string directory);
         string SaveFile(string fileData, string directory);
-        string ReadFile(string id, string directory);
     }
 
     public class JsonImageManager:IFileManager
     {
         private IFileSaver fileSaver;
         private IFileRemover imageRemover;
-        private IFileReader<string> fileReader;
 
-        public JsonImageManager(IFileSaver fileSaver, IFileRemover imageRemover, IFileReader<string> fileReader)
+        public JsonImageManager(IFileSaver fileSaver, IFileRemover imageRemover)
         {
             this.fileSaver = fileSaver;
             this.imageRemover = imageRemover;
-            this.fileReader = fileReader;
         }
 
-        public string ReadFile(string id, string directory)
-        {
-            return fileReader.ReadFileContent(directory + id + ".json");
-        }
 
         public void RemoveFile(string id, string directory)
         {
