4fed757da0dda23e117e7dfc298bcf953b8b1aee
Jakub Lotko <1elstac1@gmail.com>
2019-05-04 17:29:16 +0200

    Dodanie specyfikacji

---
+++ WebApp/Data/Specifications/TripUserByUsernameAndTripId.cs
@@ -0,0 +1,18 @@
+﻿using WebApp.Data.Entities;
+
+namespace WebApp.Data.Specifications
+{
+    public class TripUserByUsernameAndTripId : BaseSpecification<TripUser>
+    {
+        public TripUserByUsernameAndTripId(int tripId, string username) :
+            base(
+                td => td.TripId == tripId &&
+                td.User.UserName == username
+            )
+        {
+            AddInclude(td => td.User);
+            AddInclude(td => td.Trip);
+        }
+    }
+
+}
