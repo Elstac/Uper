e9df56b59a9f1408253e903ff76782b22765fbd0
Jakub Lotko <1elstac1@gmail.com>
2019-05-05 18:15:21 +0200

    Nowy provider buduje lancuch na podstawie listy

--- WebApp/Data/Specifications/Infrastructure/IncludeChainProvider.cs
+++ WebApp/Data/Specifications/Infrastructure/IncludeChainProvider.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApp.Data.Specifications.Infrastructure
+﻿using System.Collections.Generic;
+
+namespace WebApp.Data.Specifications.Infrastructure
 {
     public interface IIncludeChainProvider
     {
@@ -7,11 +9,33 @@
 
     class IncludeChainProvider : IIncludeChainProvider
     {
+        private List<BaseIncluder> includersChain;
+        private IIncluder chainStart;
+
+        public IncludeChainProvider()
+        {
+            includersChain = new List<BaseIncluder>();
+        }
+
         public IIncluder GetIncluder()
         {
-            return new TripUserCollectionIncluder(
-                new TripUserCollectionIncluder(
-                    new UserIncluder(null)));
+            if(chainStart == null)
+            {
+                for (int i = 0; i < includersChain.Count-1; i++)
+                {
+                    includersChain[i].SetNext(includersChain[i + 1]);
+                }
+                includersChain[includersChain.Count - 1].SetNext(null);
+                chainStart = includersChain[0];
+            }
+
+            return chainStart;
+        }
+
+        public IncludeChainProvider AddChain(BaseIncluder chain)
+        {
+            includersChain.Add(chain);
+            return this;
         }
     }
 }
