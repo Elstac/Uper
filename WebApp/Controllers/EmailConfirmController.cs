9bd2f26e92a2f402aa23eb97f5d23d49a12463b9
Jakub Lotko <1elstac1@gmail.com>
2019-05-06 21:48:12 +0200

    Dodanie EmailConfirmController

---
+++ WebApp/Controllers/EmailConfirmController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebApp.Models;
+using WebApp.Models.EmailConfirmation;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApp.Controllers
+{
+    public class EmailConfirmController : Controller
+    {
+        private IAccountManager accountManager;
+        private IAccountEmailConfirmatorFactory accountConfirmatorFactory;
+
+        public EmailConfirmController(
+            IAccountManager accountManager, 
+            IAccountEmailConfirmatorFactory accountConfirmatorFactory)
+        {
+            this.accountManager = accountManager;
+            this.accountConfirmatorFactory = accountConfirmatorFactory;
+        }
+
+        [Authorize]
+        [Route("[controller]/SendConfirmationEmail")]
+        public async Task<IActionResult> SendConfirmationEmailAsync(string returnUrl)
+        {
+            var user = await accountManager.GetUserAsync(HttpContext.User);
+
+            var url = Url.Action("ConfirmAccount", "Login", new { }, Request.Scheme);
+
+            await accountConfirmatorFactory
+                .CreateCofirmationSender()
+                .SendConfirmationEmailAsync(user.Id, url, user.UserName);
+
+            if(string.IsNullOrEmpty(returnUrl))
+                return RedirectToRoute("Home");
+
+            return Redirect(returnUrl);
+        }
+    }
+}
