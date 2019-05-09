c5abfccaca6864cddfdf8bbaf48486e307de67b5
Jakub Lotko <1elstac1@gmail.com>
2019-05-10 00:07:05 +0200

    Wyloczenie z pokrycia testami klas nie do tetow

--- WebApp/Exceptions/MessageException.cs
+++ WebApp/Exceptions/MessageException.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace WebApp.Exceptions
 {
+    [ExcludeFromCodeCoverage]
     public class MessageException:Exception
     {
         public MessageException(string msg):base(msg)
