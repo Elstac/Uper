64af336862883c43f006a9d4f275dda4ad66771b
Jakub Lotko <1elstac1@gmail.com>
2019-05-10 22:16:11 +0200

    Stuby pod klase i testy

---
+++ Tests/ResultNotificationHtmlProviderTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using Moq;
+using WebApp.Models.HtmlNotifications;
+
+namespace Tests
+{
+    public class ResultNotificationHtmlProviderTests
+    {
+        private HtmlNotificationProvider htmlNotificationProvider;
+
+        public ResultNotificationHtmlProviderTests()
+        {
+            htmlNotificationProvider = new HtmlNotificationProvider();
+        }
+        [Fact]
+        public void ReturnValidHtmlStringUsingGivenClassAndContent()
+        {
+
+        }
+
+    }
+}
---
+++ WebApp/Models/HtmlNotifications/HtmlNotificationProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Models.HtmlNotifications
+{
+    public interface IHtmlNotificationProvider
+    {
+        string GetNotificationBody(string pClass, string content);
+    }
+
+    public class HtmlNotificationProvider : IHtmlNotificationProvider
+    {
+        public string GetNotificationBody(string pClass, string content)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
