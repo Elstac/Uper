96db6591c2ab4484c5826569779e00941e35fcbe
Mateusz <matipio98@wp.pl>
2019-05-13 00:52:05 +0200

    poprawa chatEntryRepo

--- WebApp/Data/Repositories/ChatEntryRepository.cs
+++ WebApp/Data/Repositories/ChatEntryRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApp.Data.Entities;
+using WebApp.Data.Specifications.Infrastructure;
 
 namespace WebApp.Data.Repositories
 {
@@ -11,7 +12,7 @@ namespace WebApp.Data.Repositories
     }
     public class ChatEntryRepository : BaseRepository<ChatEntry, int>, IChatEntryRepository
     {
-        public ChatEntryRepository(ApplicationContext context) : base(context)
+        public ChatEntryRepository(ApplicationContext context, ISpecificationEvaluator specificationEvaluator) : base(context, specificationEvaluator)
         {
 
         }
