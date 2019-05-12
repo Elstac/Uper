5e49cd60b3709c725c5b50cd3e84e06db1b07360
Jakub Lotko <1elstac1@gmail.com>
2019-05-12 22:10:20 +0200

    Dodanie konkretnych implementacji managera

---
+++ WebApp/Models/FileManagement/JsonFileManager.cs
@@ -0,0 +1,23 @@
+﻿using WebApp.Models.Factories;
+
+namespace WebApp.Models.FileManagement
+{
+    public class JsonFileManager : FileManager
+    {
+        public JsonFileManager(IFileSaverFactory fileSaverFactory, IFileRemover fileRemover):
+            base(fileSaverFactory.GetSaver(SaverType.Text),fileRemover)
+        {
+
+        }
+
+        public override void RemoveFile(string id, string directory)
+        {
+            fileRemover.RemoveImage(id, directory, ".json");
+        }
+
+        public override string SaveFile(string fileData, string directory)
+        {
+            return fileSaver.SaveFile(fileData, ".json", directory);
+        }
+    }
+}
