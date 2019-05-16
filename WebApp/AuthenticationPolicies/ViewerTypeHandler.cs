376c1264827676af1468759a2ad3dd233e73e86a
Jakub Lotko <1elstac1@gmail.com>
2019-05-16 15:04:52 +0200

    Nowa polityka autoryzacji zalezna od ViewerType

---
+++ WebApp/AuthenticationPolicies/ViewerTypeHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using WebApp.Models;
+
+namespace WebApp.AuthenticationPolicies
+{
+    public class ViewerTypeHandler : AuthorizationHandler<ViewerTypeRequirement>
+    {
+        private IViewerTypeProvider viewerTypeProvider;
+
+        public ViewerTypeHandler(IViewerTypeProvider viewerTypeProvider)
+        {
+            this.viewerTypeProvider = viewerTypeProvider;
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, ViewerTypeRequirement requirement)
+        {
+            var authContext = context.Resource as AuthorizationFilterContext;
+            int id;
+            
+            if(!int.TryParse(authContext.RouteData.Values["id"].ToString(),out id))
+            {
+                if (!int.TryParse(authContext.RouteData.Values["tripId"].ToString(), out id))
+                    throw new InvalidOperationException("Invalid tripId route value name");
+            }
+
+            var viewerType = await viewerTypeProvider.GetViewerTypeAsync(context.User, id);
+
+            foreach (var type in requirement.Types)
+            {
+                if(viewerType== type)
+                {
+                    context.Succeed(requirement);
+                    break;
+                }
+            }
+        }
+    }
+}
