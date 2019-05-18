5e10ed23bda28bfc3246c2b2ac1f9bd2ed9ca901
Jakub Lotko <1elstac1@gmail.com>
2019-05-18 15:21:50 +0200

    Dodanie stubow pod nowy konwerter do VM

---
+++ WebApp/Models/TripDetailViewModelProvider/TripDetailsViewModelConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using WebApp.Data;
+using WebApp.ViewModels;
+
+namespace WebApp.Models.TripDetailViewModelProvider
+{
+    public interface ITripDetailsViewModelConverter
+    {
+        List<TripDetailsViewModel> Convert(IEnumerable<TripDetails> dataModels);
+    }
+
+    public class TripDetailsViewModelConverter : ITripDetailsViewModelConverter
+    {
+        public List<TripDetailsViewModel> Convert(IEnumerable<TripDetails> dataModels)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
