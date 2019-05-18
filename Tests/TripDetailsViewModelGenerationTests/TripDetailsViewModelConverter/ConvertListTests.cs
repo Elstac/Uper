81062fe1bce929ba8a5f21c359003d74702d3137
Jakub Lotko <1elstac1@gmail.com>
2019-05-18 15:40:23 +0200

    Pierwszy test

--- Tests/TripDetailsViewModelGenerationTests/TripDetailsViewModelConverter/ConvertListTests.cs
+++ Tests/TripDetailsViewModelGenerationTests/TripDetailsViewModelConverter/ConvertListTests.cs
@@ -1,10 +1,50 @@
-﻿using System;
+﻿using Xunit;
+using Moq;
+using WebApp.Models.TripDetailViewModelProvider;
 using System.Collections.Generic;
-using System.Text;
+using WebApp.Data;
+using WebApp.Models;
+using System;
+using WebApp.Models.Factories;
 
 namespace Tests.TripDetailsViewModelGenerationTests.TripDetailsViewModelConverterTests
 {
-    class ConvertListTests
+    public class ConvertListTests
     {
+        private TripDetailsViewModelConverter converter;
+        private Mock<ITripDetailsViewModelCreatorFactory> facMock;
+        private Mock<ITripDetailsCreator> creatorMock;
+
+        public ConvertListTests()
+        {
+            creatorMock = new Mock<ITripDetailsCreator>();
+
+            facMock = new Mock<ITripDetailsViewModelCreatorFactory>();
+            facMock.Setup(fm => fm.CreateCreator(It.IsAny<ViewerType>())).Returns(creatorMock.Object);
+
+            converter = new TripDetailsViewModelConverter(facMock.Object);
+        }
+
+        [Fact]
+        public void PassAllDataModelInGivenListToCreator()
+        {
+            var inputList = new List<TripDetails>
+                {
+                    new TripDetails(),
+                    new TripDetails(),
+                    new TripDetails(),
+                    new TripDetails()
+                };
+
+            var @out = converter.Convert(
+                inputList,
+                ViewerType.Driver
+            );
+
+            foreach (var item in inputList)
+            {
+                creatorMock.Verify(cm => cm.CreateViewModel(item));
+            }
+        }
     }
 }
--- WebApp/Models/TripDetailViewModelProvider/TripDetailsViewModelConverter.cs
+++ WebApp/Models/TripDetailViewModelProvider/TripDetailsViewModelConverter.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using WebApp.Data;
+using WebApp.Models.Factories;
 using WebApp.ViewModels;
 
 namespace WebApp.Models.TripDetailViewModelProvider
 {
     public interface ITripDetailsViewModelConverter
     {
-        List<TripDetailsViewModel> Convert(IEnumerable<TripDetails> dataModels);
+        List<TripDetailsViewModel> Convert(IEnumerable<TripDetails> dataModels, ViewerType type);
     }
 
     public class TripDetailsViewModelConverter : ITripDetailsViewModelConverter
     {
-        public List<TripDetailsViewModel> Convert(IEnumerable<TripDetails> dataModels)
+        private ITripDetailsViewModelCreatorFactory factory;
+
+        public TripDetailsViewModelConverter(ITripDetailsViewModelCreatorFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        public List<TripDetailsViewModel> Convert(IEnumerable<TripDetails> dataModels, ViewerType type)
         {
             throw new NotImplementedException();
         }
