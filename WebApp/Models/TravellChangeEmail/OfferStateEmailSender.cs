0aa20674192c764cdd7dc81e4c98fc4086feb2ba
Jakub Lotko <1elstac1@gmail.com>
2019-05-20 06:53:13 +0200

    Zmiana switcha na slownik

--- WebApp/Models/TravellChangeEmail/OfferStateEmailSender.cs
+++ WebApp/Models/TravellChangeEmail/OfferStateEmailSender.cs
@@ -1,4 +1,5 @@
-﻿using WebApp.Services;
+﻿using System.Collections.Generic;
+using WebApp.Services;
 public enum OfferStateChange
 {
     RequestAccepted,
@@ -15,27 +16,21 @@ namespace WebApp.Models.TravellChangeEmail
     public class OfferStateEmailSender : IOfferStateEmailSender
     {
         private IEmailService emailService;
+        private Dictionary<OfferStateChange, string> stateTypeDict;
 
         public OfferStateEmailSender(IEmailService emailService)
         {
             this.emailService = emailService;
+
+            stateTypeDict = new Dictionary<OfferStateChange, string>();
+            stateTypeDict.Add(OfferStateChange.UserRemoved, "RequestStateChanged");
+            stateTypeDict.Add(OfferStateChange.RequestAccepted, "RequestStateChanged");
+            stateTypeDict.Add(OfferStateChange.Deleted, "OfferStateChanged");
         }
 
         public void SendOfferStateChangedEmail(string username, OfferStateChange stateChange)
         {
-            string msgType ="";
-            switch (stateChange)
-            {
-                case OfferStateChange.RequestAccepted:
-                    msgType = "RequestStateChanged";
-                    break;
-                case OfferStateChange.UserRemoved:
-                    msgType = "RequestStateChanged";
-                    break;
-                case OfferStateChange.Deleted:
-                    msgType = "OfferStateChanged";
-                    break;
-            }
+            string msgType = stateTypeDict[stateChange];
 
             emailService.SendMail("Uper", username, msgType, null);
         }
