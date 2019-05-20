95cd71f88138de135843c2e32aa684bad0641b16
Jakub Lotko <1elstac1@gmail.com>
2019-05-20 06:45:53 +0200

    Trzy testy i implementacja

--- Tests/OfferStateEmailSender/SendOfferStateChangedEmailTests.cs
+++ Tests/OfferStateEmailSender/SendOfferStateChangedEmailTests.cs
@@ -1,14 +1,44 @@
 ﻿using Xunit;
 using Moq;
-namespace Tests.OfferStateEmailSender
+using WebApp.Models.TravellChangeEmail;
+using WebApp.Services;
+using WebApp.Models.EmailConfirmation;
+
+namespace Tests.OfferStateEmailSenderTests
 {
-    class SendOfferStateChangedEmailTests
+    public class SendOfferStateChangedEmailTests
     {
+        private OfferStateEmailSender sender;
+        private Mock<IEmailService> serviceMock;
+
         public SendOfferStateChangedEmailTests()
         {
+            serviceMock = new Mock<IEmailService>();
 
+            sender = new OfferStateEmailSender(serviceMock.Object);
         }
 
+        [Fact]
+        public void PassUserNameToEmailService()
+        {
+            sender.SendOfferStateChangedEmail("usernameX",It.IsAny<OfferStateChange>());
 
+            serviceMock.Verify(sm => sm.SendMail(It.IsAny<string>(), "usernameX", It.IsAny<string>(), It.IsAny<IMessageBodyDictionary>()));
+        }
+
+        [Fact]
+        public void PassUperToEmailServiceAsSender()
+        {
+            sender.SendOfferStateChangedEmail("usernameX", It.IsAny<OfferStateChange>());
+
+            serviceMock.Verify(sm => sm.SendMail("Uper", It.IsAny<string>(), It.IsAny<string>(), It.IsAny<IMessageBodyDictionary>()));
+        }
+        [Fact]
+        public void RequestStateChangedAsMessageTypeIfStateChangeIsAccepted()
+        {
+            sender.SendOfferStateChangedEmail("usernameX", OfferStateChange.RequestAccepted);
+
+            serviceMock.Verify(sm => sm.SendMail(It.IsAny<string>(), It.IsAny<string>(), "RequestStateChanged", It.IsAny<IMessageBodyDictionary>()));
+        }
     }
 }
--- WebApp/Models/TravellChangeEmail/OfferStateEmailSender.cs
+++ WebApp/Models/TravellChangeEmail/OfferStateEmailSender.cs
@@ -1,15 +1,41 @@
-﻿namespace WebApp.Models.TravellChangeEmail
+﻿using WebApp.Services;
+public enum OfferStateChange
+{
+    RequestAccepted,
+    UserRemoved,
+    Deleted
+}
+namespace WebApp.Models.TravellChangeEmail
 {
     public interface IOfferStateEmailSender
     {
-        void SendOfferStateChangedEmail();
+        void SendOfferStateChangedEmail(string username,OfferStateChange stateChange);
     }
 
     public class OfferStateEmailSender : IOfferStateEmailSender
     {
-        public void SendOfferStateChangedEmail()
+        private IEmailService emailService;
+
+        public OfferStateEmailSender(IEmailService emailService)
         {
-            throw new System.NotImplementedException();
+            this.emailService = emailService;
+        }
+
+        public void SendOfferStateChangedEmail(string username, OfferStateChange stateChange)
+        {
+            string msgType ="";
+            switch (stateChange)
+            {
+                case OfferStateChange.RequestAccepted:
+                    msgType = "RequestStateChanged";
+                    break;
+                case OfferStateChange.UserRemoved:
+                    break;
+                case OfferStateChange.Deleted:
+                    break;
+            }
+
+            emailService.SendMail("Uper", username, msgType, null);
         }
     }
 }
