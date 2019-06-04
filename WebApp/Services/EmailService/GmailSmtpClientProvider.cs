1bf8b4c3ef6a14440918466ecb105374aac5fcaa
Jakub Lotko <1elstac1@gmail.com>
2019-06-04 18:26:49 +0200

    Taki maly fix

---
+++ WebApp/Services/EmailService/GmailSmtpClientProvider.cs
@@ -0,0 +1,35 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using System;
+using System.Diagnostics.CodeAnalysis;
+namespace WebApp.Services
+{
+    [ExcludeFromCodeCoverage]
+    public class GmailSmtpClientProvider : ISmtpClientProvider
+    {
+        private SmtpClient client;
+
+        public void Connect(string username, string password)
+        {
+            client = new SmtpClient();
+            client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+            client.Connect("smtp.gmail.com", 465, true);
+
+            client.Authenticate(username, password);
+        }
+
+        public void Diconnect()
+        {
+            client.Disconnect(true);
+        }
+
+        public void SendMessage(MimeMessage message)
+        {
+            if (!client.IsConnected)
+                throw new InvalidOperationException("SmtpClient disconnected");
+
+            client.Send(message);
+        }
+    }
+}
